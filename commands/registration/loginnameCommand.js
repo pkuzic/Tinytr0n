8f20900c68c04e1d784ae051d3f0125fbccf142f
Elstamere <elstamere@gmail.com>
2020-12-06 22:24:49 -0500

    Bug fixes

--- commands/registration/loginnameCommand.js
+++ commands/registration/loginnameCommand.js
@@ -55,9 +55,21 @@ module.exports = class loginnameCommand extends commando.Command {
             ]
         })
     }
-    async run(commandoMsg, { text }) { 
+    async run(commandoMsg, { text }) {
+        var reg = /^[A-Za-z][A-Za-z0-9]+$/i;
+        //console.log(reg.test(text))
+        //console.log(text.length)
+        if(reg.test(text) === false) {//For obvious reasons the SQL is permitting cyrillic and special symbols, but the game can't read them. We shouldn't allow that
+            var errorExplanation = `запрещенные символы`;
+            return commandoMsg.say(`В процессе регистрации произошла ошибка: ` + "``" + `${errorExplanation}` + "``")
+        } else if (text.length < 3) {//Check if the argument is less than 3 symbols
+            var errorExplanation = `слишком короткий логин`;
+            return commandoMsg.say(`В процессе регистрации произошла ошибка: ` + "``" + `${errorExplanation}` + "``")
+        } else if (text.length > 12) {
+            var errorExplanation = `слишком длинный логин`;
+            return commandoMsg.say(`В процессе регистрации произошла ошибка: ` + "``" + `${errorExplanation}` + "``")
+        }
         discorddb.query(`SELECT stage FROM accounts WHERE memberid = ${commandoMsg.author.id}`, function (err, results, fields) {
-
             if (results[0].stage >= 2) {
                 commandoMsg.author.send(`Вы не можете пользоваться этой командой, поскольку Вы уже владеете игровым аккаунтом.`)
             } else {
@@ -69,7 +81,6 @@ module.exports = class loginnameCommand extends commando.Command {
                     //note that it always has to be converted into upper case, otherwise it won't be readable
                     //furthermore, it always should look like "LOGIN:PASSWORD"
                     var sha_password_hash = sha1(`${text.toUpperCase()}:${generatedPassword.toUpperCase()}`)
-                    
 
                     //puts the verification key into the database
                     discorddb.query(`UPDATE accounts SET verikey = "${verificationKey.toUpperCase()}" WHERE memberid = "${commandoMsg.author.id}"`)
@@ -79,18 +90,7 @@ module.exports = class loginnameCommand extends commando.Command {
                     //console.log(getActualDate())
 
                     authdb.query(`INSERT INTO account (username, sha_pass_hash, last_ip, last_attempt_ip, failed_logins, locked, lock_country, online, expansion, mutetime, locale, recruiter) VALUES ("${text.toUpperCase()}", "${sha_password_hash.toUpperCase()}", "127.0.0.1", "127.0.0.1", "0", "0", "00", "0", "2", "0", "0", "0")`, function (error, results, fields) {
-                        var reg = /^[A-Za-z][A-Za-z0-9]+$/i;
-                        //console.log(reg.test(text))
-                        //console.log(text.length)
-                        if(reg.test(text) === false) {//For obvious reasons the SQL is permitting cyrillic and special symbols, but the game can't read them. We shouldn't allow that
-                            var errorExplanation = `запрещенные символы`;
-                            return commandoMsg.say(`В процессе регистрации произошла ошибка: ` + "``" + `${errorExplanation}` + "``")
-                        }
-                        else if (text.length < 3) {//Check if the argument is less than 3 symbols
-                            var errorExplanation = `слишком короткий логин`;
-                            return commandoMsg.say(`В процессе регистрации произошла ошибка: ` + "``" + `${errorExplanation}` + "``")
-                        }
-                        else if (error) {//Check for SQL errors
+                        if (error) {//Check for SQL errors
                             var errorExplanation;
                             if (error.code === `ER_DUP_ENTRY`) {
                                 errorExplanation = `такой пользователь уже существует`;
@@ -100,14 +100,12 @@ module.exports = class loginnameCommand extends commando.Command {
                             }
                             else {
                                 errorExplanation = `неизвестная ошибка, свяжитесь с администратором, цитируя следующий код: (${error.code})`;
-                            } 
+                            }
                             return commandoMsg.say(`В процессе регистрации произошла ошибка: ` + "``" + `${errorExplanation}` + "``")
                         } else {
                             commandoMsg.say(`Отлично! Вот Ваши данные для входа на сервер игры —\nЛогин: ` + "``" + `${text.toUpperCase()}` + "``" +  `\nПароль: ` + "``" + `${generatedPassword.toUpperCase()}` + "``\nТеперь напишите в чате #регистрация ``" + config.botprefix + "verifyme``.")
                             discorddb.query(`UPDATE accounts SET stage = "2" WHERE memberid = "${commandoMsg.author.id}"`) //move to next stage (registered user)
-                            
                             authdb.query(`SELECT id FROM account WHERE username = "${text.toUpperCase()}"`, function (err, results, fields) { //get the account id from the TrinityCore auth table, thus we can use it further
-                                
                                 var accountid = results[0].id; //gets the id of the first result (according to the SQL-request there's always just one result, so no additional "just to make sure" code required)
                                 //console.log(accountid) //just to make sure that the id is right
                                 discorddb.query(`UPDATE accounts SET accountid = "${accountid}" WHERE memberid = "${commandoMsg.author.id}"`)
@@ -117,7 +115,6 @@ module.exports = class loginnameCommand extends commando.Command {
                             //this.client.guilds.cache.get(`689418791289094190`).members.resolve(`${commandoMsg.author.id}`).roles.remove(config.newbierole)
                         }
                     })
-                    
                     return;
                 }
                 else {
